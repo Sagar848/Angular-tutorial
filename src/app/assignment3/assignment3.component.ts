67688b7b6b57d829d08f0072737400dca6c144ac
Sagar Poojary <sagarpoojary1@gmail.com>
2020-02-16 23:20:14 +0530

    updated assignment 3

--- src/app/assignment3/assignment3.component.ts
+++ src/app/assignment3/assignment3.component.ts
@@ -10,16 +10,25 @@ export class Assignment3Component implements OnInit {
   buttonClickStatus = false;
   buttonClickArray = [];
   initialCountValue = 1;
+
+  buttonClickStatus2 = false;
+  buttonClickArray2 = [];
+  initialCountValue2 = 1;
   constructor() { }
 
   ngOnInit(): void {
   }
 
-
   togglePara(){
     this.initialCountValue++;
     this.buttonClickArray.push(this.initialCountValue);
     this.buttonClickStatus = true;
+  }
+
+  togglePara2(){
+    this.initialCountValue2++;
+    this.buttonClickArray2.push(new Date());
+    this.buttonClickStatus2 = !this.buttonClickStatus2;
   }
 
 }
